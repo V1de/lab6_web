5f1d11a7e67ca424566210f83950cc4f904a719b
V1de <66314615+V1de@users.noreply.github.com>
2022-12-05 17:50:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function onClick() {
+  fetch('https://randomuser.me/api')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let user = document.querySelector('.container');
+      console.log(user);
+      console.log(createUserHTML(data.results[0]));
+      user.innerHTML += createUserHTML(data.results[0]);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function createUserHTML(user) {
+  return `
+  <div class="user">
+    <img src="${user.picture.large}" alt="userPhoto">
+    <div class="info">
+      <div>Name: ${user.name.title} ${user.name.first} ${user.name.last}</div>
+      <div>City: ${user.location.city}</div>
+      <div>Country: ${user.location.country}</div>
+      <div>Postcode: ${user.location.postcode}</div>
+    </div>
+  </div>
+  `
+}
